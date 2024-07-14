8c5a9094f8dca9a8da3e0dbba3fec435c91586b9
[Gaia <[dzhayadzoy@gmail.com]>
2024-07-14 15:04:01 +0500

    created method to get class

---
+++ Main.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+
+       
+        
+       Pets cat = new Pets(0, "Felix", "cat", "male", "meow");
+        
+       System.out.println(cat);
+
+      
+
+
+    }
+
+
+
+
+}
