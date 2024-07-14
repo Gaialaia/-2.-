39eefb509f4479fc551cbf1543800b259f77bc11
[Gaia <[dzhayadzoy@gmail.com]>
2024-07-14 12:30:10 +0500

    created Pets class

--- Animals.java
+++ Animals.java
@@ -1,5 +1,102 @@
 package OOP;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 abstract class Animals {
+
+    public int id;
+    public String name;
+    public String type;
+    public String gender;
+
+    public String commands;
+
+    public Animals(int id, String name, String type, String gender, String commands) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this. gender = gender;
+        this.commands = commands;
+
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCommands (){
+        return commands;
+    }
+
+    public int setId () {
+        return id;
+    }
+
+    public String setName() {
+        return name;
+    }
+
+    public String setType(){
+        return type;
+    }
+
+    public String setGender() {
+        return gender;
+    }
+
+    public String setCommands (){
+        return commands;
+    }
+
+
+    public String toString() {
+        return "Animal [animalId=" + getId()+ ", name=" + getName() + ", type=" + super.Type()
+                + ", lastname=" + getGender() +",commands=" + getCommands() + "]";
+    }
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+  
+
+
+
+    }
     
 
-}
+
---
+++ Pets.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Pets extends Animals {
+
+    public Pets(int id, String name, String type, String gender, String commands) {
+        super(id, name, type, gender, commands);
+        
+    }
+
+    
+    
+}
